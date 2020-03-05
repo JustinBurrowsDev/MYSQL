aaa36c5b6401c143d556eb3176f01a0fa4262894
JustinBurrowsDev <58059462+JustinBurrowsDev@users.noreply.github.com>
2020-03-04 22:38:12 -0800

    Add files via upload

---
+++ movie-sqL/MYSQL.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `Movie Schema`.actors;
+INSERT INTO `Movie Schema`.actors (ActorName, DateOfBirth, CharacterName, MovieName, MovieID)
+VALUES ('Henry Cavill','May 5, 1983','Albert Mondego','Iron Man(2008)' ,2);
